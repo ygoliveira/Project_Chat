891d84ff7d8c633544fd993be21def4883658453
Ygor Oliveira <ygoliveira5@gmail.com>
2021-04-25 00:16:25 -0300

    Edit services

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,4 +1,5 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
 import { SettingsRepository } from "../repositories/SettingsRepository";
 
 
@@ -8,11 +9,14 @@ interface ISettingsCreate {
 }
 
 class SettingsService {
+    private settingsRepository: Repository<Setting>;
+
+    constructor(){
+        this.settingsRepository = getCustomRepository(SettingsRepository);
+    }
 
     async create({ chat, username }) {
-        const settingsRepository = getCustomRepository(SettingsRepository);
-
-        const userAlreadyExists = await settingsRepository.findOne({
+        const userAlreadyExists = await this.settingsRepository.findOne({
             username
         });
 
@@ -20,11 +24,11 @@ class SettingsService {
             throw new Error("User already exists!")
         }
     
-        const settings = settingsRepository.create({
+        const settings = this.settingsRepository.create({
             chat,
             username
         })
-        await settingsRepository.save(settings);
+        await this.settingsRepository.save(settings);
 
         return settings;
     }
--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,12 +1,18 @@
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
 import { UsersRepository } from '../repositories/UsersRepository';
 
 
 class UsersService {
+    private usersRepository: Repository<User>;
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository)
+    }
+
     async create(email: string) {
-        const usersRepository = getCustomRepository(UsersRepository)
         // Verifica se usuário existe, caso não salva no banco de dados
-        const userExists = await usersRepository.findOne({
+        const userExists = await this.usersRepository.findOne({
             email
         })
 
@@ -14,14 +20,13 @@ class UsersService {
             return userExists;
         }
 
-        const user = usersRepository.create({
+        const user = this.usersRepository.create({
             email
         });
 
-        await usersRepository.save(user);
+        await this.usersRepository.save(user);
 
         return user;
-
     }
 }
 
